36426d5db53e97acbbac5d887d150fc92322c08a
michaeltorres1 <torres173@connect.wcsu.edu>
2019-10-01 15:36:49 -0700

    ../src/classes/

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import _ from 'lodash';
+import landing from './classes/landing';
+// TODO: test style sheet is just for testing
 import test from './assets/stylesheets/test.scss';
 
 function component () {
