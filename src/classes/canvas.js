97d3ac87c4d2ab71776e6981a2effb86c458d51c
michaeltorres1 <torres173@connect.wcsu.edu>
2019-10-03 17:02:51 -0700

    Add gameloop

--- src/classes/canvas.js
+++ src/classes/canvas.js
@@ -1,37 +1,82 @@
-let canvas = document.getElementById('canvas');
+let canvas = document.querySelector('canvas');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
 let ctx = canvas.getContext('2d');
-const GAME_WIDTH = canvas.width;
-const GAME_HEIGHT = canvas.height;
 
-// let canvas = document.getElementById('canvas');
-// let ctx = canvas.getContext('2d');
-// var x = canvas.width / 8;
-// var y = canvas.height;
-// var dx = 2;
-// var dy = -2;
-// var speedX = 0;
-// var speedY = 0;
-// var gravity = 0.05;
-// var gravitySpeed = 0;
+function Fruit(x, y) {
+  this.x = x;
+  this.y = y;
+  this.dx = randomX(x);
+  this.dy = -4;
+  this.visibility = true;
 
-// function drawFruit() {
-//   ctx.beginPath();
-//   ctx.arc(x, y, 10, 0, Math.PI * 2); // to make circle
-//   ctx.fillStyle = "#0095DD"; // fill circle with color
-//   ctx.fill();
-//   ctx.closePath();
-// }
+  this.draw = function() {
+    ctx.beginPath();
+    ctx.arc(x, y, 30, 0, Math.PI * 2, false);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+  }
 
-// function draw() {
-//   ctx.clearRect(0, 0, canvas.width, canvas.height);
-//   drawFruit();
-//   x += dx;
+  this.update = function() {
+    let randomY = Math.random() * ( (150 - 0) + 0);
+    
+    if (y < randomY) {
+      this.dy += 0.5863;
+    }
 
-//   if (y === canvas.height - 20) {
-//     dy *= -1;
-//   }
-//   y += dy;
-// }
+    if (y > canvas.height + 100) {
+      this.visibility = false;
+    }
 
-// setInterval(draw, 10);
-// // requestAnimationFrame(draw);
+    // if (y > (canvas.height + 40)) {
+    //   ctx.clearRect(0, 0, innerWidth, innerHeight);
+    //   y = canvas.height;
+    //   this.dy = -4;
+    // }
+
+    x += this.dx;
+    y += this.dy;
+
+    // this.draw();
+  }
+}
+
+function randomX(x) {
+  if (x > ( canvas.width / 2 )) {
+    return Math.random() * (0 - 3) + 0;
+  } else {
+    return Math.random() * (3 - (0)) + 1;
+  }
+}
+
+// logic to save fruits in array
+let fruits = [];
+
+for (let i = 0; i < 10; i++) {
+  let x = Math.random() * innerWidth;
+  let y = canvas.height;
+
+  fruits.push(new Fruit(x, y));
+}
+
+function animate() {
+  ctx.clearRect(0, 0, innerWidth, innerHeight);
+  
+  for (let i = 0; i < fruits.length; i++) {
+    if (fruits[i].visibility === false) {
+      let x = Math.random() * innerWidth;
+      let y = canvas.height;
+
+      fruits[i] = new Fruit(x, y);
+    }
+
+    fruits[i].update();
+    fruits[i].draw();
+  }
+
+  requestAnimationFrame(animate);
+}
+
+animate();
